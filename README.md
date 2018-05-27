c32931ea56745970aa72225d661152bd135e2f35
Adam Linkous <alinkous@gmail.com>
2018-05-27 15:38:44 -0500

    Add generated cookbook content

---
+++ README.md
@@ -0,0 +1,4 @@
+# mywrapper_chef-slack_handler
+
+TODO: Enter the cookbook description here.
+
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook:: mywrapper_chef-slack_handler
+# Recipe:: default
+#
+# Copyright:: 2018, The Authors, All Rights Reserved.
